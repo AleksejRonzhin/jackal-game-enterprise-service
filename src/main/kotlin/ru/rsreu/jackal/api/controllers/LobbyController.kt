1827979969d3b32c583a2622f531db4630e2569a
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2022-07-14 20:05:30 +0300

    JG-21 Added swagger documentation

--- src/main/kotlin/ru/rsreu/jackal/api/controllers/LobbyController.kt
+++ src/main/kotlin/ru/rsreu/jackal/api/controllers/LobbyController.kt
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import ru.rsreu.jackal.api.services.LobbyService
 import ru.rsreu.jackal.api.controllers.dto.CreateLobbyClientRequest
+import ru.rsreu.jackal.api.services.LobbyService
 import ru.rsreu.jackal.shared_models.responses.CreateLobbyResponse
 
 @RestController
 @RequestMapping("/api/lobby")
 class LobbyController(val service: LobbyService) {
     @PostMapping("/create")
-    fun create(@RequestBody request: CreateLobbyClientRequest, authentication: Authentication): ResponseEntity<CreateLobbyResponse> {
-        return ResponseEntity.ok(service.create(request, authentication.principal.toString().toLong()))
+    fun create(
+        @RequestBody request: CreateLobbyClientRequest,
+        authentication: Authentication
+    ): ResponseEntity<CreateLobbyResponse> {
+        return ResponseEntity.ok(
+            service.create(
+                request.lobbyName,
+                request.lobbyPassword,
+                authentication.principal.toString().toLong()
+            )
+        )
     }
 }
--- src/main/kotlin/ru/rsreu/jackal/api/services/LobbyService.kt
+++ src/main/kotlin/ru/rsreu/jackal/api/services/LobbyService.kt
@@ -3,7 +3,6 @@ package ru.rsreu.jackal.api.services
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForEntity
-import ru.rsreu.jackal.api.controllers.dto.CreateLobbyClientRequest
 import ru.rsreu.jackal.configuration.LobbyServiceConfiguration
 import ru.rsreu.jackal.shared_models.requests.CreateLobbyServiceRequest
 import ru.rsreu.jackal.shared_models.responses.CreateLobbyResponse
@@ -13,10 +12,11 @@ class LobbyService(
     val restTemplate: RestTemplate,
     val lobbyServiceConfiguration: LobbyServiceConfiguration
 ) {
-    fun create(request: CreateLobbyClientRequest, hostId: Long) = restTemplate.postForEntity<CreateLobbyResponse>(
-        lobbyServiceConfiguration.lobbyServiceUrl + lobbyServiceConfiguration.lobbyCreationUrlPart,
-        CreateLobbyServiceRequest(
-            request.lobbyName, request.lobbyPassword, hostId
-        )
-    ).body
+    fun create(lobbyName: String, lobbyPassword: String?, hostId: Long) =
+        restTemplate.postForEntity<CreateLobbyResponse>(
+            lobbyServiceConfiguration.lobbyServiceUrl + lobbyServiceConfiguration.lobbyCreationUrlPart,
+            CreateLobbyServiceRequest(
+                lobbyName, lobbyPassword, hostId
+            )
+        ).body
 }
