c5205cd7d146ff2ac80883d9359e6f66cbdb0c85
maximastashkin <maksim.astash@gmail.com>
2022-07-15 17:39:35 +0300

    CreateLobbyResponse status fix

--- src/main/kotlin/ru/rsreu/jackal/shared_models/responses/CreateLobbyResponse.kt
+++ src/main/kotlin/ru/rsreu/jackal/shared_models/responses/CreateLobbyResponse.kt
@@ -6,8 +6,8 @@ import ru.rsreu.jackal.shared_models.WebSocketInfo
 data class CreateLobbyResponse(
     val webSocketInfo: WebSocketInfo? = null,
     val token: String? = null,
-    override val responseStatus: JoinLobbyStatus
-) : ResponseBody<JoinLobbyStatus>
+    override val responseStatus: CreateLobbyStatus
+) : ResponseBody<CreateLobbyStatus>
 
 enum class CreateLobbyStatus {
     OK, HOST_ALREADY_IN_LOBBY, NOT_UNIQUE_LOBBY_TITLE
