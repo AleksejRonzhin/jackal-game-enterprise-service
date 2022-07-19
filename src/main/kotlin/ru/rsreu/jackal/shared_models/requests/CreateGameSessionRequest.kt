2caf931660e1ffd1288cd7f4d6e13fab5f8b793d
maximastashkin <maksim.astash@gmail.com>
2022-07-20 00:34:04 +0300

    Contracting

--- src/main/kotlin/ru/rsreu/jackal/shared_models/requests/CreateGameSessionRequest.kt
+++ src/main/kotlin/ru/rsreu/jackal/shared_models/requests/CreateGameSessionRequest.kt
@@ -2,6 +2,6 @@ package ru.rsreu.jackal.shared_models.requests
 
 data class CreateGameSessionRequest(
     val lobbyId: String,
-    val userIds: Collection<Long>,
-    val gameModeTitle: String
+    val usersIds: Collection<Long>,
+    val gameMode: String
 )
