5ffa9d05a5efac8e09d9a2d454de1b475df3e69e
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2022-07-20 20:49:20 +0300

    Added default game mode for lobby

--- src/main/resources/db/migration/V2__Init.sql
+++ src/main/resources/db/migration/V2__Init.sql
@@ -2,3 +2,9 @@ INSERT INTO permissions (permission) VALUES
 ('USER'),
 ('ADMIN'),
 ('UNBLOCKED');
+
+INSERT INTO games (id, title, client_address, service_address) VALUES
+(0, 'Jackal', 'http://localhost:3000/', 'http://localhost:8030/' );
+
+INSERT INTO game_modes (id, title, game_id, min_player_number, max_player_number) VALUES
+(0, 'DEFAULT', 0, 4, 4);
